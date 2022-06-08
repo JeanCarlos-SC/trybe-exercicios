d345284dd7186e2cc73d8c123a1ec4d38602809c
Jean Carlos <jncorreia44@gmail.com>
2022-06-08 18:48:29 -0300

    modificando concatenção para template literal - exercicio 1

--- 1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-01.js
+++ 1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-01.js
@@ -1,7 +1,7 @@
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
       const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
@@ -10,4 +10,4 @@ const testingScope = (escopo) => {
     // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
-testingScope(false);
+testingScope(true);
