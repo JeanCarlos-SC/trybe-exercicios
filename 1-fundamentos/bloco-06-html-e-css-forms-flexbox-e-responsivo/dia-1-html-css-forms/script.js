6f5b7b07bda2b1a9b6d3de3fecd0deabc59a302e
Jean Carlos <jncorreia44@gmail.com>
2022-07-07 20:52:01 -0300

    Finalizando criação e validações do formulario

--- 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById('submit');
 const check = document.getElementById('checkbox2');
-const inputName = document.getElementById('input-name').value;
-const inputEmail = document.getElementById('input-email').value;
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
 const validations = (event) => {
-    if (inputName.length < 10 || inputName.length > 40) {
+    if (inputName.value.length < 10 || inputName.value.length > 40) {
         event.preventDefault();
         alert('Dados Inválidos');
-    } else if (inputEmail.length < 10 || inputEmail.lenght > 50) {
+    } else if (inputEmail.value.length < 10 || inputEmail.value.lenght > 50) {
         event.preventDefault();
         alert('Dados Inválidos');
     } else if (!check.checked) {
