b8342b0d00bde6d0c8fd8fa32e70ebad32874c72
Jean Carlos <jncorreia44@gmail.com>
2022-07-12 16:15:21 -0300

    FEAT: utilizando a lib Just-validate para realizar as validações necessárias no formulário e a lib DatePickerX para validação do campo para colocar a data

--- 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javaScript-e-frameworks-css/script.js
+++ 1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javaScript-e-frameworks-css/script.js
@@ -1,19 +1,51 @@
-const btn = document.getElementById('submit');
-const check = document.getElementById('checkbox2');
-const inputName = document.getElementById('input-name');
-const inputEmail = document.getElementById('input-email');
-const validations = (event) => {
-    if (inputName.value.length < 10 || inputName.value.length > 40) {
-        event.preventDefault();
-        alert('Dados Inválidos');
-    } else if (inputEmail.value.length < 10 || inputEmail.value.lenght > 50) {
-        event.preventDefault();
-        alert('Dados Inválidos');
-    } else if (!check.checked) {
-        event.preventDefault();
-        alert('È necessário aceitar o uso das imagens para enviar o formulário');
-    } else {
-        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-    }
+const datePicker = document.getElementById('datepicker').DatePickerX.init({format: 'dd/mm/yyyy'});
+
+
+//Usando biblioteca Just-Validate para validações no formulário
+const validation = new JustValidate('#form');
+validation
+.addField('#input-name', [
+{
+rule: 'minLength',
+value: 10,
+errorMessage: 'Dados Inválidos!'
+},
+{
+rule: 'maxLength',
+value: 40,
+errorMessage: 'Dados Inválidos!'
+},
+])
+.addField('#input-email', [
+{
+rule: 'minLength',
+value: 10,
+errorMessage: 'Dados Inválidos!',
+},
+{
+rule: 'maxLength',
+value: 50,
+errorMessage: 'Dados Inválidos!',
+},
+{
+rule: 'email',
+errorMessage: 'Email Inválido!!',
+},
+])
+.addField('#checkbox2', [
+{
+rule: 'required',
+errorMessage: 'È necessário aceitar o uso das imagens para enviar o formulário',
 }
-btn.addEventListener('click', validations)
+])
+.addField('#textarea-answer', [
+{
+rule: 'maxLength',
+value: 500,
+errorMessage: 'Tamanho máximo de 500 caracteres!!',
+}
+])
+.onSuccess((event) => {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip', event);
+    location.reload();
+  });
